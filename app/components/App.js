0784bb039935ff87bc336cf878962f2fb5891923
Vuong Vu Nguyen <vuongvu333@gmail.com>
2017-06-01 00:25:33 +0700

    react-router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import Popular from './Popular';     
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';    
+import Battle from './Battle';    
+import Popular from './Popular'; 
 
 class App extends Component {
   render() {
     return(
-      <div>
-        <Popular /> 
-      </div>
+      <BrowserRouter>
+        <div className='container'>
+          <Nav />
+        
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={() => {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
